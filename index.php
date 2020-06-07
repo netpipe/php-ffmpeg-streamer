d0171bee4d4208213cc6d43ebcedd0cdfdaf70a5
teslaenergy@gmail.com <teslaenergy@gmail.com>
2020-06-06 22:39:51 -0600

    inital

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html xml:lang="en" lang="en">
+<head>
+    <meta name="Keywords" content="keywords" /><meta name="Description" content="Grand Gallery" />
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="refresh" content="25">
+    <!--should be set higher for less traffic use DEVELOPMENT MODE-->
+    
+    <title>Grand Gallery</title>
+    
+</head>
+
+<body style="background:#968D87">
+ <div id="wrapper">
+
+<?php
+	//if ( $fileinfo['extension'] == "webm" ) {
+	//echo '<video controls style="" preload="none" loop="1" poster="play.png" width="300">';
+	//echo '<source src="' . $file . '"' .'type="video/webm";codecs="vp8, vorbis" />'; 
+	//echo '</video>';
+?>
+
+ <video autoplay width="1000" height="740" controls>
+  <source src="output0.webm" type="video/webm">
+Your browser does not support the video tag.
+</video> 
+
+  </div> <!--wrapper-->
+  
+
+</body>
+</html>
