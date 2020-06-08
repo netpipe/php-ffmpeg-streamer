c7da39c3bb088f85f5758cd88121cc443f16acad
teslaenergy@gmail.com <teslaenergy@gmail.com>
2020-06-08 13:59:51 -0600

    php

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta http-equiv="refresh" content="25">
     <!--should be set higher for less traffic use DEVELOPMENT MODE-->
-    
+        <!-- example usage http://localhost/php-ffmpeg-streamer/?user=admin -->
+
     <title>Grand Gallery</title>
     
 </head>
@@ -15,49 +16,35 @@
  <div id="wrapper">
 
 <?php
+
+        $user=$_GET["user"];
+
 	//if ( $fileinfo['extension'] == "webm" ) {
 	//echo '<video controls style="" preload="none" loop="1" poster="play.png" width="300">';
 	//echo '<source src="' . $file . '"' .'type="video/webm";codecs="vp8, vorbis" />'; 
 	//echo '</video>';
-echo "hi";
-
-$files = array_diff( scandir("./FTP-client/08-06-2020/"), array(".", "..") );
-
-print_r($files); // just for debugging
-
+$dir = "./FTP-client/08-06-2020/";
+//$user = "admin";
+$dir = "./software/" . $user . "/" . date("d-m-Y");// date("d/m/Y") 
+echo $dir;
+$files = array_diff( scandir($dir), array(".", "..") );
+//print_r($files); // just for debugging
 sort($files);
-
-foreach($files as $item) {
-
- echo "$item";
-
-}
-
-
-//$dir = "./FTP-client/08-06-2020/";
-//$exclude = array( ".","..","error_log","_notes" );
-//if (is_dir($dir)) {
-//    $files = scandir($dir);
-//    foreach($files as $file){
-//        if(!in_array($file,$exclude)){
-          //  echo '<li><img src="' . $dir . $file . '" /></li>';
-//           echo $file;
- //       }
-//    }
+//foreach($files as $item) {
+// echo "$item";
 
 //}
-
-
+//echo next($files) . "<br>";
+//echo  "$dir . end($files)";
+echo '<video autoplay width="1000" height="740" controls>';
+echo '<source src="' . $dir . end($files) . '" type="video/webm">';
+echo 'your browser does not support the video tag.';
+echo '</video>';
+//echo '<source src="' . "./software/" . date("d/m/Y") . $dir . end($files) . '" type="video/webm">';
 
 //var vid = document.getElementById("video1");
 //alert(vid.duration); 
 ?>
-
- <video autoplay width="1000" height="740" controls>
-  <source src="output0.webm" type="video/webm">
-Your browser does not support the video tag.
-</video> 
-
 
   </div> <!--wrapper-->
   
