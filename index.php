81de05fde9a625363c3e12a1cd81bee71381f04d
teslaenergy@gmail.com <teslaenergy@gmail.com>
2020-06-08 12:05:14 -0600

    phpfix

--- index.php
+++ index.php
@@ -19,12 +19,45 @@
 	//echo '<video controls style="" preload="none" loop="1" poster="play.png" width="300">';
 	//echo '<source src="' . $file . '"' .'type="video/webm";codecs="vp8, vorbis" />'; 
 	//echo '</video>';
+echo "hi";
+
+$files = array_diff( scandir("./FTP-client/08-06-2020/"), array(".", "..") );
+
+print_r($files); // just for debugging
+
+sort($files);
+
+foreach($files as $item) {
+
+ echo "$item";
+
+}
+
+
+//$dir = "./FTP-client/08-06-2020/";
+//$exclude = array( ".","..","error_log","_notes" );
+//if (is_dir($dir)) {
+//    $files = scandir($dir);
+//    foreach($files as $file){
+//        if(!in_array($file,$exclude)){
+          //  echo '<li><img src="' . $dir . $file . '" /></li>';
+//           echo $file;
+ //       }
+//    }
+
+//}
+
+
+
+//var vid = document.getElementById("video1");
+//alert(vid.duration); 
 ?>
 
  <video autoplay width="1000" height="740" controls>
   <source src="output0.webm" type="video/webm">
 Your browser does not support the video tag.
 </video> 
+
 
   </div> <!--wrapper-->
   
